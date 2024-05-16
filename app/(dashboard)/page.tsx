26719c28257db680d24d1c1b5298b5ca89d5186f
JarGad23 <j.gad@m8b.pl>
2024-05-16 17:47:37 +0200

    Layout and page change

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,24 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
+  const { data: accounts, isLoading, isError } = useGetAccounts();
+
+  if (isLoading) {
+    return <div>Is Loading... </div>;
+  }
+
   return (
     <Button>
       <UserButton />
+      <div>
+        {accounts?.map((account) => (
+          <p>{account.name}</p>
+        ))}
+      </div>
     </Button>
   );
 }
