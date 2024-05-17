fcd93725fde6042a1c40fce07c96d7907d2c34d1
JarGad23 <j.gad@m8b.pl>
2024-05-17 15:54:08 +0200

    Added toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { QueryProvider } from "@/providers/query-provider";
 import { SheetProvider } from "@/providers/sheet-provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,6 +25,7 @@ export default function RootLayout({
           <QueryProvider>
             <SheetProvider />
             {children}
+            <Toaster />
           </QueryProvider>
         </body>
       </html>
