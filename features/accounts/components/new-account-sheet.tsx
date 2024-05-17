df10e2c9880657afd9d2166ec9c363e833cc7c9d
Jarosław Gad <127743272+JarGad23@users.noreply.github.com>
2024-05-17 15:54:58 +0200

    Feature 8: Create account form (#8)
    * Bun lock package json changes
    
    * Added toaster
    
    * Added account form and hook to operate mutations
    
    * Added form to sheet

--- features/accounts/components/new-account-sheet.tsx
+++ features/accounts/components/new-account-sheet.tsx
@@ -1,3 +1,4 @@
+import { z } from "zod";
 import {
   Sheet,
   SheetContent,
@@ -6,9 +7,24 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { AccountForm } from "./account-form";
+import { insertAccountSchema } from "@/db/schema";
+import { useCreateAccount } from "../api/use-create-account";
+
+const formSchema = insertAccountSchema.pick({ name: true });
+
+type FormValues = z.input<typeof formSchema>;
 
 export const NewAccountSheet = () => {
   const { isOpen, onClose } = useNewAccount();
+
+  const mutatuion = useCreateAccount();
+
+  const onSubmit = (values: FormValues) => {
+    mutatuion.mutate(values, {
+      onSuccess: () => onClose(),
+    });
+  };
 
   return (
     <Sheet open={isOpen} onOpenChange={onClose}>
@@ -19,6 +35,11 @@ export const NewAccountSheet = () => {
             Create a new account to track your transactions.
           </SheetDescription>
         </SheetHeader>
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutatuion.isPending}
+          defaultValues={{ name: "" }}
+        />
       </SheetContent>
     </Sheet>
   );
