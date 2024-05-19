5d4f861bd14736dfbb5b42aa2d4b755cad297075
JarGad23 <j.gad@m8b.pl>
2024-05-19 15:04:26 +0200

    Changes in route and account components

--- app/(dashboard)/accounts/page.tsx
+++ app/(dashboard)/accounts/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete";
+import { useBulkDeleteAccounts } from "@/features/accounts/api/use-bulk-delete-accounts";
 import { Loader2, Plus } from "lucide-react";
 import { DataTable } from "@/components/data-tabel";
 import { columns } from "./_components/columns";
