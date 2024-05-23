2f413c861c5d400ec7453e264501d5d810953298
JarGad23 <j.gad@m8b.pl>
2024-05-23 17:46:10 +0200

    Improved dashboard page and added dashboard cards

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,27 +1,9 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-import { UserButton } from "@clerk/nextjs";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { DataGrid } from "@/components/data-grid";
 
 export default function Home() {
-  const { onOpen } = useNewAccount();
-  const { data: accounts, isLoading, isError } = useGetAccounts();
-
-  if (isLoading) {
-    return <div>Is Loading... </div>;
-  }
-
   return (
-    <div>
-      <UserButton />
-      <Button onClick={onOpen}>Add an Account</Button>
-      <div>
-        {accounts?.map((account) => (
-          <p>{account.name}</p>
-        ))}
-      </div>
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
     </div>
   );
 }
